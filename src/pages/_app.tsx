e0b4abf0c5950f3670231337ef714f76a2f134ae
seoky <sseok@springKingui-MacBookPro.local>
2025-05-20 00:42:47 +0900

    feat: global layout 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Link from "next/link";
+import GlobalLayout from "@/components/global-layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   
   
   return (
     <>
-      <header>
-        <Link href={'/'}>index</Link>
-        &nbsp;
-        <Link href={'/search'}>search</Link>
-        &nbsp;
-        <Link href={'/book/1'}>book1</Link>
-      </header>
-      <Component {...pageProps} />
+      <GlobalLayout>
+        <Component {...pageProps} />
+      </GlobalLayout>
     </>
   )
 }
