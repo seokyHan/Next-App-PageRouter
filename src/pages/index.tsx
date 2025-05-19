c088d39cd6d42cbcf6affc61187ad1426128d486
seoky <sseok@springKingui-MacBookPro.local>
2025-05-20 00:29:45 +0900

    feat: css style api routes test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import style from './index.module.css'
+
 export default function Home() {
   return (
     <>
-      <h1>index</h1>
+      <h1 className={style.h1}>index</h1>
+      <h2 className={style.h2}>H2</h2>
     </>
   );
 }
