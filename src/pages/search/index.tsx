aa511a4701ec786495e526b58a9a780cc1e64e51
seoky <sseok@springKingui-MacBookPro.local>
2025-05-20 23:00:45 +0900

    feat: ui 구현

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,13 +1,17 @@
 import { useRouter } from "next/router"
 import SearchableLayout from "@/components/searchable-layout";
 import { ReactNode } from "react";
+import books from '@/mock/books.json'
+import BookItem from "@/components/book-item";
 
 export default function Page() {
-  const router = useRouter();
-  const {q} = router.query;
 
   return (
-      <h1>Search {q}</h1>
+      <div>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </div>
   )
 }
 
