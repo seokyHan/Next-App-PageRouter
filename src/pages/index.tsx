122ebb801447e88759f0ef0007e93f6b34c7de7d
seoky <sseok@springKingui-MacBookPro.local>
2025-05-20 23:41:37 +0900

    feat: SSR 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,42 @@
 import style from './index.module.css'
 import SearchableLayout from '@/components/searchable-layout';
 import { ReactNode } from 'react';
-import books from '@/mock/books.json'
 import BookItem from '@/components/book-item';
+import { InferGetServerSidePropsType } from 'next';
+import fetchBooks from '@/lib/fetch-books';
+import fetchRandomBooks from '@/lib/fetch-random-books';
 
-export default function Home() {
+export const getServerSideProps = async() => {
+  const [allbooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks()
+  ]);
+
+  return {
+    props: {
+      allbooks,
+      recoBooks
+    }
+  }
+}
+
+export default function Home({
+  allbooks, 
+  recoBooks
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  
   return (
     <>
       <div className={style.container}>
         <section>
           <h3>지금 추천하는 도서</h3>
-          {books.map((book) => (
+          {recoBooks.map((book) => (
             <BookItem key={book.id} {...book} />
           ))}
         </section>
         <section>
           <h3>등록된 모든 도서</h3>
-          {books.map((book) => (
+          {allbooks.map((book) => (
             <BookItem key={book.id} {...book} />
           ))}
         </section>
